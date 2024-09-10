df681c4c7ea3d94f446ca2fe9e80547f378f2407
Balivada Karunakar <karunakarbalivada786@gmail.com>
2024-09-10 10:55:50 +0530

    fix : middleware

--- server/middlewares/helperMiddleware.js
+++ server/middlewares/helperMiddleware.js
@@ -3,7 +3,7 @@ export const helperMiddleware =  async (req,res,next) => {
     try{
         const user=await Users.findById(req.userId);
         if (user.accountType !== 'helper'){
-            return res.status(401).json({"message":"only can be uploaded by tutor"})
+            return res.status(401).json({"message":"only can be done by helper"});
         }
         else next();
     }
